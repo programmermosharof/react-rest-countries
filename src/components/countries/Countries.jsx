0a59308149844670321ddd6652f097f0b076d0e8
programmermosharof <mosharof.dev@gmail.com>
2026-03-15 10:47:04 +0600

    Project Staert

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>My Name Is React JS</h2>
+            
+        </div>
+    );
+};
+
+export default Countries;
